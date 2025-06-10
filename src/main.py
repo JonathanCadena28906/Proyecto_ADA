a6dd8d4af98dad51944e769b2d9b283601c4d17b
JonathanC <142516406+JonathanCadena28906@users.noreply.github.com>
2025-06-10 01:36:43 -0500

    Update main.py para implementar las estrategias geometricas

--- src/main.py
+++ src/main.py
@@ -1,26 +1,35 @@
+import os
+import json
+import time
+import pandas as pd
+from openpyxl import load_workbook
 from src.controllers.manager import Manager
 
+
+from src.controllers.manager import Manager
 from src.controllers.strategies.force import BruteForce
-from src.controllers.strategies.q_nodes_paralela import QNodesParallel
 from src.controllers.strategies.q_nodes import QNodes
+from src.controllers.strategies.geometric_secuencial import GeometricSia
+from src.controllers.strategies.geometric_paralelo import GeometricSiaParallel
+from src.controllers.strategies.phi import Phi
 
 
 def iniciar():
     """Punto de entrada principal"""
                     # ABCD #
-    estado_inicial = "10000000000000000000"  # 1: activo, 0: inactivo
-    condiciones =    "11111111111111111111"
-    alcance =        "11111111111111111111"  # 1: activo, 0: inactivo
-    mecanismo =      "11111111111111111111"
+    estado_inicial = "100000000000000"  # Estado inicial del sistema
+    condiciones =    "111111111111111"  # Condiciones del sistema
+    alcance =        "111111111111111"  # Alcance de la solución
+    mecanismo =      "111111111111111"  # Mecanismo de solución
 
     gestor_sistema = Manager(estado_inicial)
-    #Manager.generar_red(Manager, dimensiones=20)
 
     ### Ejemplo de solución mediante módulo de fuerza bruta ###
-    analizador_fb = QNodesParallel(gestor_sistema)
+    analizador_fb = GeometricSiaParallel(gestor_sistema)
     sia_uno = analizador_fb.aplicar_estrategia(
         condiciones,
         alcance,
         mecanismo,
     )
     print(sia_uno)
+    
